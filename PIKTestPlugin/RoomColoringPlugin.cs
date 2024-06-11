81438dab447886a2f5dd90aaeb42b05a7cf892d4
hyperoman <surgu4ev.m@yandex.ru>
2024-06-11 08:43:40 +0300

    Убраны бесполезные using

--- PIKTestPlugin/RoomColoringPlugin.cs
+++ PIKTestPlugin/RoomColoringPlugin.cs
@@ -1,18 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Autodesk.Revit.UI.Selection;
 using Autodesk.Revit.DB.Architecture;
-using System.Runtime.ExceptionServices;
-using System.Diagnostics;
-using System.Xml.Linq;
-using System.IO;
 
 namespace RoomColoringPlugin
 {
