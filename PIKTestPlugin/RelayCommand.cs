87fc56f175f0f9e1572de878a248e1c6e80e052f
hyperoman <surgu4ev.m@yandex.ru>
2024-06-17 12:06:36 +0300

    Добавлены комментарии и убрано форматирование

--- PIKTestPlugin/RelayCommand.cs
+++ PIKTestPlugin/RelayCommand.cs
@@ -1,34 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-
-namespace PIKTestPlugin
+﻿namespace PIKTestPlugin
 {
+    using System;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// Класс для создания команд, которые могут быть привязаны к элементам управления в WPF.
+    /// Реализует интерфейс ICommand.
+    /// </summary>
     public class RelayCommand : ICommand
     {
-        private Action<object> execute;
-        private Func<object, bool> canExecute;
+        // Поля
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
 
+        // Конструкторы
+
+        /// <summary>
+        /// Конструктор класса RelayCommand.
+        /// </summary>
+        /// <param name="execute">Действие, выполняемое командой.</param>
+        /// <param name="canExecute">Функция, определяющая, может ли команда выполняться.</param>
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        // События
+
+        /// <summary>
+        /// Событие, вызываемое при изменении состояния выполнения команды.
+        /// </summary>
         public event EventHandler CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
         }
 
-        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
-        {
-            this.execute = execute;
-            this.canExecute = canExecute;
-        }
+        // Методы
 
+        /// <summary>
+        /// Определяет, может ли команда выполняться.
+        /// </summary>
+        /// <param name="parameter">Параметр команды.</param>
+        /// <returns>true, если команда может выполняться; в противном случае — false.</returns>
         public bool CanExecute(object parameter)
         {
             return this.canExecute == null || this.canExecute(parameter);
         }
 
+        /// <summary>
+        /// Выполняет команду.
+        /// </summary>
+        /// <param name="parameter">Параметр команды.</param>
         public void Execute(object parameter)
         {
             this.execute(parameter);
