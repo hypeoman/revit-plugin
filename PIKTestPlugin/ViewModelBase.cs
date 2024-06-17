e126554822c7568a0bfcbb7e256e756743998d43
hyperoman <surgu4ev.m@yandex.ru>
2024-06-17 12:02:00 +0300

    Добавлены комментарии и исправлено форматирование.

--- PIKTestPlugin/ViewModelBase.cs
+++ PIKTestPlugin/ViewModelBase.cs
@@ -1,11 +1,22 @@
 ﻿using System.ComponentModel;
 
+/// <summary>
+/// Базовый класс ViewModel, реализующий интерфейс INotifyPropertyChanged.
+/// Этот класс используется для оповещения представления (View) о изменениях свойств модели (Model).
+/// </summary>
 public class ViewModelBase : INotifyPropertyChanged
-{
+    {
+    /// <summary>
+    /// Событие, вызываемое при изменении значения свойства.
+    /// </summary>
     public event PropertyChangedEventHandler PropertyChanged;
 
+    /// <summary>
+    /// Метод, вызывающий событие PropertyChanged.
+    /// </summary>
+    /// <param name="propertyName">Имя измененного свойства.</param>
     protected void OnPropertyChanged(string propertyName)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 }
