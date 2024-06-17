f2dc0cba39cb210cad690504bcb0f2350aee8658
hyperoman <surgu4ev.m@yandex.ru>
2024-06-17 07:06:50 +0300

    Файл кода для главной формы.

---
+++ PIKTestPlugin/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using PIKTestPlugin;
+using System.Linq.Expressions;
+using System.Windows;
+
+namespace RoomColoringPlugin
+{
+    public partial class MainWindow : Window
+    {
+        public ApplicationViewModel _viewModel;
+        private ExternalAnalyzeHandler _externalEventHandler;
+        private ExternalEvent _externalEvent;
+
+        public MainWindow(UIApplication uiApp, UIDocument uiDoc, Document doc)
+        {
+            InitializeComponent();
+            _viewModel = new ApplicationViewModel(uiApp, uiDoc, doc);
+            DataContext = _viewModel;
+
+            _externalEventHandler = new ExternalAnalyzeHandler(_viewModel);
+            _externalEvent = ExternalEvent.Create(_externalEventHandler);
+        }
+    }
+}
